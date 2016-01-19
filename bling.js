f779ed4656a39ff92e7a3a54741a004e5f665ea2
danray <dray@healthwise.org>
2016-01-18 17:07:42 -0700

    first release

---
+++ bling.js
@@ -0,0 +1,42 @@
+//
+// for all your console bling needs
+//
+var pak = require('./package.json');
+exports.hr = function(char, this_many) {
+  console.log(this.repeatSoMany(char, this_many));
+};
+
+exports.repeatIt = function(char, string_to_match) {
+  return this.repeatSoMany(char, string_to_match.length);
+};
+
+exports.repeatSoMany = function(char, this_many) {
+  var a = '';
+  for (var i = 0; i < this_many; i++) {
+    a = a + char;
+  }
+  return a;
+};
+
+exports.box = function(a_string, char, how_wide) {
+  var a = this.repeatSoMany(char, how_wide);
+  var b = a + ' ' + a_string + ' ' + a;
+  console.log(this.repeatIt(char, b));
+  console.log(b);
+  console.log(this.repeatIt(char, b));
+};
+
+exports.style1 = function(a_string) {
+    var b = this;
+    var how_wide = a_string.length;
+    b.hr(' | ',10);
+    b.hr('_',13 + how_wide);
+    b.box(a_string, '@',6);
+    b.hr('~',13 + how_wide);
+    b.hr(' | ',10);
+};
+
+//usage example
+var b = this;
+b.style1('BLING! by antfriend');
+b.box('version:' + pak.version, '*',3);
